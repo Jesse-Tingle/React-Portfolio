cec2c385b8e425ee7aab65fe74340d5fee14a8f9
Jesse-Tingle <jtingle0@gmail.com>
2020-07-28 23:43:07 -0400

    updates project form component

--- portfolio/src/components/projects/ProjectForm.js
+++ portfolio/src/components/projects/ProjectForm.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 function ProjectForm() {
-	return <div></div>;
+	handleImageChange = (e) => {
+		const image = e.target.files[0];
+		const formData = new FormData();
+		formData.append("image", image, image.name);
+	};
+
+	const uploadImage = (formData) => {
+		axios.post("/project/image", formData).then((res) => {});
+	};
+
+	return (
+		<div>
+			<input type="file" id="image-upload" onChange={handleImageChange} />
+		</div>
+	);
 }
 
 export default ProjectForm;
