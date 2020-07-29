21968e95f3f1a99422c3e222532d269bb6ddeabe
Jesse-Tingle <jtingle0@gmail.com>
2020-07-28 23:24:57 -0400

    removes server files and start over with blank ProjectForm.js component

--- portfolio/src/components/projects/ProjectForm.js
+++ portfolio/src/components/projects/ProjectForm.js
@@ -1,52 +1,7 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 
 function ProjectForm() {
-	const [selectedFile, setSelectedFile] = useState({
-		selectedFile: null,
-	});
-
-	const fileSelectedHandler = (event) => {
-		setSelectedFile({
-			selectedFile: event.target.files[0],
-		});
-	};
-
-	const fileUploadHandler = (event) => {
-		const fd = new FormData();
-		fd.append("image", selectedFile, selectedFile.name);
-		axios.post("http://http://localhost:3001/upload", fd).then((res) => {
-			console.log("res", res);
-		});
-	};
-
-	return (
-		<div className="container">
-			<h1>File Upload</h1>
-			{typeof msg != "undefined" ? msg : ""}
-			<form action="/upload" method="POST" encType="multipart/form-data">
-				<div className="file-field input-field">
-					<div className="btn grey">
-						<span>File</span>
-						<input name="myImage" type="file" />
-					</div>
-					<div className="file-path-wrapper">
-						<input className="file-path validate" type="text" />
-					</div>
-				</div>
-				<button className="btn" type="submit">
-					Submit
-				</button>
-			</form>
-			<br />
-			<img
-				src={typeof file != "undefined" ? file : ""}
-				// src="<%= typeof file != 'undefined' ? file : '' %>"
-				className="responsive-img"
-				alt=""
-			/>
-		</div>
-	);
+	return <div></div>;
 }
 
 export default ProjectForm;
